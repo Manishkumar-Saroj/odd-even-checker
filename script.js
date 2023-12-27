43c1922f736a4103f728c1861096b644584d28af
Manishkumar-Saroj <126165524+Manishkumar-Saroj@users.noreply.github.com>
2023-12-27 16:43:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function check() {
+    var numInput = document.getElementById("numInput");
+    var resultElement = document.getElementById("result");
+    var num = numInput.value.trim();
+
+    if (num === "") {
+        // If input is empty, clear the result
+        resultElement.innerHTML = "";
+        resultElement.style.backgroundColor = "";
+    } else if (num % 2 == 0) {
+        resultElement.innerHTML = "Even Number";
+        resultElement.style.backgroundColor = "lightcoral";
+    } else {
+        resultElement.innerHTML = "Odd Number";
+        resultElement.style.backgroundColor = "lightgreen";
+    }
+}
+
+function clearInput() {
+    var numInput = document.getElementById("numInput");
+    var resultElement = document.getElementById("result");
+
+    numInput.value = '';
+    resultElement.innerHTML = '';
+    resultElement.style.backgroundColor = '';
+}
+
+function handleScroll(event) {
+    var numInput = document.getElementById("numInput");
+    var delta = event.deltaY;
+
+    if (delta > 0) {
+        // Scroll down, decrease the number
+        numInput.stepDown();
+    } else {
+        // Scroll up, increase the number
+        numInput.stepUp();
+    }
+
+    // Trigger the check function after updating the value
+    check();
+}
+
+function incrementValue() {
+    var numInput = document.getElementById("numInput");
+    numInput.stepUp();
+    check();
+}
+
+function decrementValue() {
+    var numInput = document.getElementById("numInput");
+    numInput.stepDown();
+    check();
+}
